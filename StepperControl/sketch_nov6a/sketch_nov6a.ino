7f9e66d9a8877e30bde12965e45f72dd830b8cf3
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2023-11-06 12:32:57 -0800

    Update sketch_nov6a.ino

--- StepperControl/sketch_nov6a/sketch_nov6a.ino
+++ StepperControl/sketch_nov6a/sketch_nov6a.ino
@@ -11,7 +11,10 @@
  
 // change this to the number of steps on your motor
 #define STEPS 32
- 
+int function(){
+  std::cout << "tough";
+  return 0;
+}
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
 // attached to
