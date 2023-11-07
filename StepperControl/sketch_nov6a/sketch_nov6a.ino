a63aa1a5d7821ae5ec44517eceeffd05d6db107b
Trevor <63212388+TR-05@users.noreply.github.com>
2023-11-06 16:11:49 -0800

    Update sketch_nov6a.ino

--- StepperControl/sketch_nov6a/sketch_nov6a.ino
+++ StepperControl/sketch_nov6a/sketch_nov6a.ino
@@ -6,24 +6,31 @@
 
 Stepper stepper(STEPS, 8, 10, 9, 11);
 
-const int button = 4;  // direction control button is connected to Arduino pin 4
+const int user_input_button = 4;  // direction control button is connected to Arduino pin 4
+const int position_reset_button = 6;  // direction control button is connected to Arduino pin 4
+
 const int pot = A0;    // speed control potentiometer is connected to analog pin 0
 
+float position = 0;
+float target_angle = 0;
+float stepsToDeg() {
+  return position * 360/32;
+}
 void setup() {
   // configure button pin as input with internal pull up enabled
-  pinMode(button, INPUT_PULLUP);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+  pinMode(user_input_button, INPUT_PULLUP);
+  pinMode(position_reset_button, INPUT_PULLUP);
 }
 
 int direction_ = 1, speed_ = 0;
-
+int last_user_input = 0;
 void loop() {
-  if (digitalRead(button) == 0)  // if button is pressed
-    if (debounce())              // debounce button signal
-    {
-      direction_ *= -1;  // reverse direction variable
-      while (debounce())
-        ;  // wait for button release
-    }
+  int user_input = digitalRead(user_input_button);
+  if (user_input == 0) {
+    target_angle += 90;
+  }
+  last_user_input = user_input;
 
   // read analog value from the potentiometer
   int val = analogRead(pot);
@@ -35,16 +42,39 @@ void loop() {
     // set the speed of the motor
     stepper.setSpeed(speed_);
   }
-
+  if (digitalRead(position_reset_button)) 
+  position = 0;
   // move the stepper motor
   stepper.step(direction_);
+  position += direction_;
+  print();
+  
+}
+
+void print() {
+    // print labels
+  Serial.print(stepsToDeg());  // prints a label
+  Serial.print(" degrees");         // prints a tab
+  Serial.println();        // carriage return after the last label
 }
 
 // a small function for button debounce
 bool debounce() {
   byte count = 0;
   for (byte i = 0; i < 5; i++) {
-    if (digitalRead(button) == 0)
+    if (digitalRead(user_input_button) == 0)
+      count++;
+    delay(10);
+  }
+  if (count > 2) return 1;
+  else return 0;
+}
+
+// a small function for button debounce
+bool resetCheck() {
+  byte count = 0;
+  for (byte i = 0; i < 5; i++) {
+    if (digitalRead(position_reset_button) == 0)
       count++;
     delay(10);
   }
